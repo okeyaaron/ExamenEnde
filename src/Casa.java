4b5691d7af075330d5a68ac00dae3d1bed95efc8
okeyaaron <48756086+okeyaaron@users.noreply.github.com>
2019-05-24 13:31:47 +0200

    Creacion Casa
    En este git hemos creado la clase java con todos sus metodos

---
+++ src/Casa.java
@@ -0,0 +1,19 @@
+
+public class Casa {
+
+	private float superficie;
+	private String calle;
+	private int codigoPostal;
+	
+	
+	
+	/**
+	 * Un equipo de limpiadores deja la casa libre de germenes y desinfecta las superficies
+	 */
+	
+	public void limpiarLaCasa() {
+		String mensajito="Te hemos limpiado la casa, esta desinfectada";
+		System.out.println(mensajito);
+	}
+	
+}
