c27fae20f634e8cf15d50945f0e168510c8cc1f9
okeyaaron <48756086+okeyaaron@users.noreply.github.com>
2019-05-24 13:51:51 +0200

    Los ladrones
    Los ladrones han entrado en la casa y roban todo

--- src/Casa.java
+++ src/Casa.java
@@ -8,11 +8,11 @@ public class Casa {
 	
 	
 	/**
-	 * Un equipo de limpiadores deja la casa libre de germenes y desinfecta las superficies
+	 * Un equipo de ladrones deja la casa limpia y roba hasta el cobre de las paredes
 	 */
 	
 	public void limpiarLaCasa() {
-		String mensajito="Te hemos limpiado la casa, esta desinfectada";
+		String mensajito="Te hemos limpiado la casa, estas arruinado";
 		System.out.println(mensajito);
 	}
 	
